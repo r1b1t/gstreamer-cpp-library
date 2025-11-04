40eb388127ec362ada75bbbbdbbcb928ff39c6b0
Berk <nberkunal10@gmail.com>
2025-11-04 10:32:45 +0300

    gitignore

--- examples/pngtovideo.cpp
+++ examples/pngtovideo.cpp
@@ -3,6 +3,12 @@
 #include "PipelineBuilder.h"
 #include <iostream>
 
+// PDF dosyasını PNG resimlerine dönüştürür ve ardından bu PNG dosyalarından bir video oluşturur.
+// Fps değeri düşük ve video süresi her sayfa için 5 saniye olacak şekilde ayarlanmıştır.
+// Giriş PDF dosyası: "pngtovideo_output/pdf_input/example4pages.pdf"
+// Çıkış PNG dosyaları: "pngtovideo_output/output_frames/frame_0.png", "frame_1.png", ...
+// Çıkış video dosyası: "pngtovideo_output/lowfps20second.mp4"
+
 int main(int argc, char *argv[])
 {
     // PDF dosyasını PNG resimlerine dönüştürme
--- examples/pngtovideo2.cpp
+++ examples/pngtovideo2.cpp
@@ -3,6 +3,13 @@
 #include "PipelineBuilder.h"
 #include <iostream>
 
+// PDF dosyasını PNG resimlerine dönüştürür ve ardından bu PNG dosyalarından bir video oluşturur.
+// Fps değeri yüksek (25 fps) ve video süresi her sayfa için
+// 5 saniye olacak şekilde ayarlanmıştır. Bunu başarmak için,
+// her sayfa için 125 (5s * 25FPS) frame kopyalanır.
+// Çıkış PNG dosyaları: "pngtovideo2_output/output_frames/frame_0.png", "frame_1.png", ...
+// Çıkış video dosyası: "pngtovideo2_output/25fps20second.mp4"
+
 int main(int argc, char *argv[])
 {
     // PDF dosyasını PNG resimlerine dönüştürme
